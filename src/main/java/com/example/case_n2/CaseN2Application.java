95dd13620fda982a37ffbf63521dd29e98ea6f00
chien2002 <chienad2002ad@gmail.com>
2023-10-03 15:49:24 +0700

    init

---
+++ src/main/java/com/example/case_n2/CaseN2Application.java
@@ -0,0 +1,13 @@
+package com.example.case_n2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaseN2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CaseN2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/case_n2/CaseN2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.case_n2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaseN2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
